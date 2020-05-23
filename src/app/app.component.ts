3dc3fa13fac150a10500a3edce7cf365dbf3236d
ansumanmishra <sunmoonfortune@gmail.com>
2020-05-23 20:51:55 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+
+import * as mobilenet from '@tensorflow-models/mobilenet';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-tensorflow-image-detection';
+  @ViewChild('image', {read: ElementRef, static: false}) image: ElementRef;
+  @ViewChild('imagePrediction', {read: ElementRef, static: false}) imagePrediction: ElementRef;
+  showPredictionLoading = false;
+  onChangeImage(event: any ) {
+
+    const image = event.target.files[0];
+
+    if(image) {
+      const reader = new FileReader();
+      reader.readAsDataURL(image);
+
+      reader.onload = async (res: any) => {
+        this.image.nativeElement.src = res.target.result;
+        this.showPredictionLoading = true;
+        const model = await mobilenet.load();
+        const predictions = await model.classify(this.image.nativeElement);
+        this.imagePrediction.nativeElement.innerHTML = `Is it a ${predictions[0].className}?`;
+      }
+    }
+  }
 }
