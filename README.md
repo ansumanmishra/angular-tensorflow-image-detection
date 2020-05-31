d354d3a4419e01ca72b9ef46674da859d776d44f
ansumanmishra <sunmoonfortune@gmail.com>
2020-06-01 00:08:39 +0200

    Fixing styling and updating README.md

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# AngularTensorflowImageDetection
+# Angular Tensorflow Image Detection
 
-Predict uploaded image. Built using Angular and Tensorflow js
+## Predict uploaded image. Built using Angular and Tensorflow js
+
 url - https://quizzical-carson-0c48bf.netlify.app/
+
+### To run locally:
+* Clone the project
+* Do 'npm install'
+* Run 'npm start'
+* Open the url 'http://localhost:4200' in the browser
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,40 @@ import * as mobilenet from '@tensorflow-models/mobilenet';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="container">
+      <h1>
+        <span translate="no">
+          <span style="color:#FF0000">P</span>
+          <span style="color:#FF8000">R</span>
+          <span style="color:#FFFF00">E</span>
+          <span style="color:#007940">D</span>
+          <span style="color:#4040FF">I</span>
+          <span style="color:#A000C0">C</span>
+          <span style="color:#FF0000">T</span>
+        </span>
+      </h1>
+      <h2 #imagePrediction [hidden]="!showPredictionLoading">Predicting 🤔 ...</h2>
+      <input id="file-input" type="file" (change)="onChangeImage($event)" />
+      <div>
+        <label for="file-input">
+          <img src="./assets/upload-image.png" alt="Click to upload..." title="Click to upload..." #image width="40%" height="auto">
+        </label>
+      </div>
+      <footer>@copyright</footer>
+    </div>
+  `,
 })
 export class AppComponent {
   title = 'angular-tensorflow-image-detection';
   @ViewChild('image', {read: ElementRef, static: false}) image: ElementRef;
   @ViewChild('imagePrediction', {read: ElementRef, static: false}) imagePrediction: ElementRef;
   showPredictionLoading = false;
-  onChangeImage(event: any ) {
 
+  onChangeImage(event: any) {
     const image = event.target.files[0];
 
-    if(image) {
+    if (image) {
       const reader = new FileReader();
       reader.readAsDataURL(image);
 
